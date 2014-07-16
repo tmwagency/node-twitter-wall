a947e3c8a2a1142dfcda032e8ccf3a80730e0691
Ashley Nolan <anolan1@anolan1.local>
2014-07-16 18:00:56 +0100

    Update of how config is stored

--- config/config-twitter.js
+++ config/config-twitter.js
@@ -1,9 +1,9 @@
 module.exports = {
 	local : {
-		consumer_key: process.env['TWITTER_CONSUMER_KEY'],
-		consumer_secret: process.env['TWITTER_CONSUMER_SECRET'],
-		access_token_key: process.env['TWITTER_ACCESS_TOKEN'],
-		access_token_secret: process.env['TWITTER_ACCESS_SECRET']
+		consumer_key: process.env['TWITTER_CONSUMER_KEY'] || require('./privconfig')['local']['consumer_key'],
+		consumer_secret: process.env['TWITTER_CONSUMER_SECRET'] || require('./privconfig')['local']['consumer_secret'],
+		access_token_key: process.env['TWITTER_ACCESS_TOKEN'] || require('./privconfig')['local']['access_token_key'],
+		access_token_secret: process.env['TWITTER_ACCESS_SECRET'] || require('./privconfig')['local']['access_token_secret'],
 	},
 	dev : {
 		consumer_key: '',
